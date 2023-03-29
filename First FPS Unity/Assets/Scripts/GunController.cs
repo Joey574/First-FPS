0a884a33340db6e64ed7e7ba6a8cb35d85d60083
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2023-03-28 21:24:53 -0700

    Added gun model
    Baretta M9 gun model w/ recoil, next comes an m4

--- First FPS Unity/Assets/Scripts/GunController.cs
+++ First FPS Unity/Assets/Scripts/GunController.cs
@@ -8,26 +8,41 @@ public class GunController : MonoBehaviour
     public KeyCode shoot = KeyCode.Mouse0;
     public KeyCode aim = KeyCode.Mouse1;
     public KeyCode toggleFire = KeyCode.V;
-    public KeyCode reload = KeyCode.r;
+    public KeyCode reload = KeyCode.R;
 
     private bool readyToShoot = true;
+    private bool reloading = false;
     private bool auto = true;
 
     [Header("Gun Data")]
     public float fireRate;
     public float bulletSpeed;
-    public int ammo = 12;
+    public int maxAmmo;
+    public int ammo;
     public float ejectVelocity;
-    public float recoil;
-    public float horizontalSway;
+    public float verticalRecoil;
+    public float horizontalRecoil;
     public float verticalSway;
+    public float horizontalSway;
+    public float reloadTime;
+    public bool canAuto;
     public float volume;
-    
+
+    public GameObject gun;
+    public GameObject camera;
     public GameObject bulletPrefab;
     public GameObject casingPrefab;
-    public Transform bulletSpan;
+    public Transform bulletSpawn;
     public Transform casingSpawn;
 
+    private PlayerCam cameraRecoil;
+    private Animation anim;
+
+    private void Awake()
+    {
+        cameraRecoil = camera.GetComponent <PlayerCam> ();
+        anim = gun.GetComponent<Animation>();
+    }
 
     private void Update()
     {
@@ -36,38 +51,84 @@ public class GunController : MonoBehaviour
             auto = !auto;
         }
 
-        if (Input.GetKeyUp(shoot))
+        if (Input.GetKeyDown(reload))
         {
-            resetShoot();
+            reloading = true;
+            startReload();
         }
 
-        if (Input.GetKey(shoot) && readyToShoot && auto)
+        if (!reloading)
         {
-            readyToShoot = false;
-            spawnBullet();
-            Invoke(nameof(resetShoot), (60f / fireRate));
-            
-        } else if (Input.GetKey(shoot) && readyToShoot && !auto) {
-            spawnBullet();
-            readyToShoot = false;
+            if (canAuto && auto)
+            {
+                if (Input.GetKey(shoot) && readyToShoot && auto)
+                {
+                    readyToShoot = false;
+                    spawnBullet();
+                    Invoke(nameof(resetShoot), (60f / fireRate));
+
+                }
+            }
+            else
+            {
+                if (Input.GetKeyUp(shoot))
+                {
+                    resetShoot();
+                }
+
+                else if (Input.GetKey(shoot) && readyToShoot)
+                {
+                    spawnBullet();
+                    readyToShoot = false;
+                }
+            }
         }
     }
 
     private void spawnBullet()
     {
         if (ammo > 0) {
-             var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.forward * bulletSpeed;
-            var casing = Instantiate(casingPrefab, casingSpawn.position, casingSpawn.rotation);
-            casing.GetComponent<Rigidbody>().velocity = casingSpawn.right * ejectVelocity;
             ammo--;
+            var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.forward * bulletSpeed;
+            anim.Play("kickBack");
+
+            cameraRecoil.addRecoil(verticalRecoil, horizontalRecoil);
+
+            Invoke(nameof(spawnCasing), 0.08f);
         } else {
             
         }
     }
 
+    private void spawnCasing()
+    {
+        var casing = Instantiate(casingPrefab, casingSpawn.position, casingSpawn.rotation);
+        casing.GetComponent<Rigidbody>().velocity = casingSpawn.up * ejectVelocity;
+    }
+
     private void resetShoot()
     {
+        StartCoroutine(WaitForAnimation(anim));
+    }
+
+    private void startReload()
+    {
+        Invoke(nameof(setAmmoToMax), reloadTime);
+    }
+
+    private void setAmmoToMax()
+    {
+        ammo = maxAmmo;
+        reloading = false;
+    }
+
+    private IEnumerator WaitForAnimation(Animation animation)
+    {
+        while(animation.isPlaying)
+        {
+            yield return null;
+        }
         readyToShoot = true;
     }
 }
--- First FPS Unity/Assets/Scripts/PlayerCam.cs
+++ First FPS Unity/Assets/Scripts/PlayerCam.cs
@@ -8,6 +8,9 @@ public class PlayerCam : MonoBehaviour
     public float sensX;
     public float sensY;
 
+    public float verticalRecoil = 0;
+    public float horizontalRecoil = 0;
+
     public Transform orientation;
     public Transform player;
 
@@ -24,14 +27,24 @@ public class PlayerCam : MonoBehaviour
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        
 
-        yRotation += mouseX;
+        yRotation += mouseX - horizontalRecoil;
 
-        xRotation -= mouseY;
+        xRotation -= mouseY + verticalRecoil;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        horizontalRecoil = 0;
+        verticalRecoil = 0;
 
         transform.rotation = UnityEngine.Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = UnityEngine.Quaternion.Euler(0, yRotation, 0);
         player.rotation = UnityEngine.Quaternion.Euler(0, yRotation, 0);
+    }
+
+    public void addRecoil(float v, float h)
+    {
+        verticalRecoil += v;
+        horizontalRecoil += h;
     }
 }
